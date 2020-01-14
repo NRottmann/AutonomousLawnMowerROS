a0e61332074f6c74fd53b78d9e6a470a98392130
Nils Rottmann <Nils.Rottmann@rob.uni-luebeck.de>
2020-01-14 16:56:55 +0100

    Add Readme

--- README.md
+++ README.md
@@ -1,14 +1,42 @@
-# ROS Autonomous Lawn Mower (under current development)
-ROS Stack for the self-designed autonomous lawn mower of the University of Lübeck
+# Autonomous Lawn Mower - ROS
+ROS Stack for the self-designed autonomous lawn mower of the University of Luebeck. We aim to design a fully autonomous lawn mower equipped with chlorophyll sensors developed at our Institute. We show that intelligent behavior with only limited sensing is possible.
 
 ## Requirements
 * ROS Kinetic
+* Gazebo V7.0 (comes with the full-Desktop Version of ROS Kinetic)
+* Eigen3
 
-## What does this project do:
-* It enables the autonomous lawn mower to naviagte completely autonomously
+## Packages
+* simulation_environment: Simulation Environment Package based on Gazebo for testing purposes
+* interfaces: Package for interfaces between ROS and the sensors and actuators of the real robot, e.g. the chlorophyll sensors, the IMU, the motor
+* localization: Package with localization algorithm such as a particle or a kalman filter
+* mapping: Package for the mapping algorithm, where based on a wall following controller a pose graph is generated which is updated due to detected loop closures
+* control: Control Package with different control algorithm, such as a PID Controller or a Wall Follower 
 
-## Testen (zB mit MATLAB R2018b)
-```
-roscore ...
-```
+## How to get started
+- Start by creating a catkin workspace folder and downloading the git repository
+
+  ```bash
+  mkdir -p ~/mower_ws/src
+  cd ~/mower_ws/src
+  git clone https://github.com/NRottmann/AutonomousLawnMowerROS
+  ```
+
+- Compile the catkin workspace
+
+  ```bash
+  cd ~/mower_ws
+  catkin_make
+  ```
+
+- Run the mapping algorithm by first sourcing your packages and then starting the launch script
+
+  ```bash
+  source devel/setup.bash
+  roslaunch mapping mapping_simulation.launch
+  ```
+
+- It should open then a Gazebo environment which shows the robot cycling along the boundary of the closed environment, similar to the picture below
+
+  ![](images/ExampleMapping.jpg)
 
--- mapping/src/pgOptimization.cpp
+++ mapping/src/pgOptimization.cpp
@@ -24,10 +24,14 @@ class Listener
 		Listener(ros::NodeHandle nh, ros::NodeHandle nhp);										// this is the constructor
 	private:
 		// Storage capacity for the pose graph information, TODO: Change to dynamic arrays?
-		MatrixXf PG;
+		MatrixXd PG;
+		MatrixXd PG_opt;
 		MatrixXi LC;
 		unsigned int idx_PG;
 		unsigned int idx_LC;
+
+		// ...
+		double dx;
 
 		// Subscriber and Publisher
 		ros::Subscriber sub_PG;
@@ -35,9 +39,9 @@ class Listener
 		ros::Publisher pub_Map;
 
 		// functions
-		Vector3f getRelMeasurement(Vector3f x_i, Vector3f x_j);
-		Matrix3f getJacobianA(Vector3f z, Vector3f p_i, Vector3f p_j);
-		Matrix3f getJacobianB(Vector3f z, Vector3f p_i, Vector3f p_j);
+		Vector3d getRelMeasurement(Vector3d x_i, Vector3d x_j);
+		Matrix3d getJacobianA(Vector3d z, Vector3d p_i, Vector3d p_j);
+		Matrix3d getJacobianB(Vector3d z, Vector3d p_i, Vector3d p_j);
 		void optimizePoseGraph();
 		nav_msgs::OccupancyGrid generateGridMap();
 };
@@ -46,6 +50,9 @@ class Listener
 Listener::Listener(ros::NodeHandle nh, ros::NodeHandle nhp) {
 	idx_PG = 0;
 	idx_LC = 0;
+
+	// Initialize variables
+	dx = pow(10,-9);
 
 	// Initialize subscribers
 	sub_PG = nh.subscribe("poseGraph", 1000, &Listener::callbackPG, this);
@@ -58,19 +65,23 @@ Listener::Listener(ros::NodeHandle nh, ros::NodeHandle nhp) {
 void Listener::callbackPG(const mapping::PG::ConstPtr& msg_in)
 {
 	mapping::PG msg_tmp = *msg_in;
-	Vector3f new_PG; new_PG << msg_tmp.x, msg_tmp.y, msg_tmp.phi;
+	Vector3d new_PG; new_PG << msg_tmp.x, msg_tmp.y, msg_tmp.phi;
 
 	PG.conservativeResize(3,idx_PG+1);
+	PG_opt = MatrixXd::Zero(3,idx_PG+1);
+
 	PG.col(idx_PG) = new_PG;
+	PG_opt = PG;
+
 	idx_PG++;
 
 	if (idx_LC > 1) {
 		ROS_INFO("pgOptimization: Start Pose Graph Optimization!");
 		optimizePoseGraph();
 		ROS_INFO("pgOptimization: Pose Graph Optimization finished!");
-		nav_msgs::OccupancyGrid map = generateGridMap();
-		pub_Map.publish(map);
 	}
+	nav_msgs::OccupancyGrid map = generateGridMap();
+	pub_Map.publish(map);
 }
 void Listener::callbackLC(const mapping::LC::ConstPtr& msg_in)
 {
@@ -85,78 +96,86 @@ void Listener::callbackLC(const mapping::LC::ConstPtr& msg_in)
 // Function for optimizing the pose graph
 void Listener::optimizePoseGraph() {
 	// Define error matrices, TODO: Change that according to the paper!
-	float sigma = 0.1;
-	Matrix3f Omega; Omega << sigma, 0, 0, 0, sigma, 0, 0, 0, sigma;
+	double sigma = 0.1;
+	Matrix3d Omega; Omega << sigma, 0, 0, 0, sigma, 0, 0, 0, sigma;
 
 	// Generate relative odometry measurements
-	MatrixXf Z; Z = MatrixXf::Zero(3,idx_PG-1);
+	MatrixXd Z; Z = MatrixXd::Zero(3,idx_PG-1);
 	for(int i=1; i<idx_PG; i++) {
 		Z.block(0,i-1,3,1) = getRelMeasurement(PG.col(i-1), PG.col(i));
 	}
 
 	// Iterate until convergence
-	MatrixXf PG_opt = PG;
-	float err = 1000000;
+	double err = 1000000;
 	int iter = 0;
 	while(err > 0.001 && iter < 100) {
-		VectorXf b(3*idx_PG); b = VectorXf::Zero(3*idx_PG);
-		MatrixXf H(3*idx_PG,3*idx_PG); H = MatrixXf::Zero(3*idx_PG,3*idx_PG);
+		VectorXd b(3*idx_PG); b = VectorXd::Zero(3*idx_PG);
+		MatrixXd H(3*idx_PG,3*idx_PG); H = MatrixXd::Zero(3*idx_PG,3*idx_PG);
 		// Add odometry measurements
 		for(int i=1; i<idx_PG; i++) {
-			Matrix3f A = getJacobianA(Z.col(i-1), PG_opt.col(i-1), PG_opt.col(i));
-			Matrix3f B = getJacobianB(Z.col(i-1), PG_opt.col(i-1), PG_opt.col(i));
+			Matrix3d A = getJacobianA(Z.col(i-1), PG_opt.col(i-1), PG_opt.col(i));
+			Matrix3d B = getJacobianB(Z.col(i-1), PG_opt.col(i-1), PG_opt.col(i));
 			H.block(i-1,i-1,3,3) += A.transpose() * Omega * A;
 			H.block(i-1,i,3,3) += A.transpose() * Omega * B;
 			H.block(i,i-1,3,3) += B.transpose() * Omega * A;
 			H.block(i,i,3,3) += B.transpose() * Omega * B;
 
-			Vector3f e = Z.col(i-1) - getRelMeasurement(PG_opt.col(i-1), PG_opt.col(i));
+			Vector3d e = Z.col(i-1) - getRelMeasurement(PG_opt.col(i-1), PG_opt.col(i));
 			b.segment(i-1,3) += A.transpose() * Omega * e;
 			b.segment(i,3) += B.transpose() * Omega * e;
+
+			// ROS_INFO("PG_opt:\n %f, %f, %f", PG_opt(0,i-1), PG_opt(1,i-1), PG_opt(2,i-1));
+			// ROS_INFO("A:\n %f, %f, %f \n %f, %f, %f \n %f, %f, %f", A(0,0), A(0,1), A(0,2), A(1,0), A(1,1), A(1,2), A(2,0), A(2,1), A(2,2));
+			// ROS_INFO("B:\n %f, %f, %f \n %f, %f, %f \n %f, %f, %f", B(0,0), B(0,1), B(0,2), B(1,0), B(1,1), B(1,2), B(2,0), B(2,1), B(2,2));
 		}
 		// Add loop closure measurements
 		for(int i=0; i<idx_LC; i++) {
-			Vector3f z_LC = Vector3f::Zero();
+			Vector3d z_LC = Vector3d::Zero();
 			int idx_i = LC(0,i)-1;
 			int idx_j = LC(1,i)-1;
 
-			Matrix3f A = getJacobianA(z_LC, PG_opt.col(idx_i), PG_opt.col(idx_j));
-			Matrix3f B = getJacobianB(z_LC, PG_opt.col(idx_i), PG_opt.col(idx_j));
+			Matrix3d A = getJacobianA(z_LC, PG_opt.col(idx_i), PG_opt.col(idx_j));
+			Matrix3d B = getJacobianB(z_LC, PG_opt.col(idx_i), PG_opt.col(idx_j));
 			H.block(idx_i,idx_i,3,3) += A.transpose() * Omega * A;
 			H.block(idx_i,idx_j,3,3) += A.transpose() * Omega * B;
 			H.block(idx_j,idx_i,3,3) += B.transpose() * Omega * A;
 			H.block(idx_j,idx_j,3,3) += B.transpose() * Omega * B;
 
-			Vector3f e = z_LC - getRelMeasurement(PG_opt.col(idx_i), PG_opt.col(idx_j));
+			Vector3d e = z_LC - getRelMeasurement(PG_opt.col(idx_i), PG_opt.col(idx_j));
 			b.segment(idx_i,3) += A.transpose() * Omega * e;
 			b.segment(idx_j,3) += B.transpose() * Omega * e;
 		}
 		// Keep the first node fixed
-		H.block(0,0,3,3) += Matrix3f::Identity();
+		H.block(0,0,3,3) += Matrix3d::Identity();
 		// Solve the problem
-		VectorXf dx = H.colPivHouseholderQr().solve(-b);
+		VectorXd dx = H.colPivHouseholderQr().solve(b);
 		// Determine correction error
 		err = dx.norm();
+		iter++;
+		double rel_err = (H*dx + b).norm() / b.norm();
+		ROS_INFO("n_norm: %f", b.norm());
+		ROS_INFO("rel_err: %f", rel_err);
+		ROS_INFO("err: %f", err);
 		// Update the pose
-		MatrixXf PG_add; PG_add = MatrixXf::Zero(3,idx_PG);
+		MatrixXd PG_add; PG_add = MatrixXd::Zero(3,idx_PG);
 		for(int i=0; i<idx_PG; i++) {
 			PG_add.col(i) = dx.segment(3*i,3);
 		}
-		PG += PG_add;
+		PG_opt += PG_add;
 	}
 }
 
 // Calculate Jacobian
-Matrix3f Listener::getJacobianA(Vector3f z, Vector3f p_i, Vector3f p_j) {
+Matrix3d Listener::getJacobianA(Vector3d z, Vector3d p_i, Vector3d p_j) {
 	// Define variables
-	float dx = powf(10,-9);
-	Matrix3f J = Matrix3f::Zero();
+	Matrix3d J = Matrix3d::Zero();
 
 	// Get the current error
-	Vector3f e = z - getRelMeasurement(p_i, p_j);
+	Vector3d e = z - getRelMeasurement(p_i, p_j);
+	// ROS_INFO("e: %f, %f, %f", e(0), e(1), e(2));
 
 	// Get the jacobian
-	Vector3f p_i_temp;
+	Vector3d p_i_temp;
 	for(int i=0; i<3; i++) {
 		p_i_temp = p_i;
 		p_i_temp(i) = p_i_temp(i) + dx;
@@ -167,16 +186,15 @@ Matrix3f Listener::getJacobianA(Vector3f z, Vector3f p_i, Vector3f p_j) {
 }
 
 // Calculate Jacobian
-Matrix3f Listener::getJacobianB(Vector3f z, Vector3f p_i, Vector3f p_j) {
+Matrix3d Listener::getJacobianB(Vector3d z, Vector3d p_i, Vector3d p_j) {
 	// Define variables
-	float dx = powf(10,-9);
-	Matrix3f J = Matrix3f::Zero();
+	Matrix3d J = Matrix3d::Zero();
 
 	// Get the current error
-	Vector3f e = z - getRelMeasurement(p_i, p_j);
+	Vector3d e = z - getRelMeasurement(p_i, p_j);
 
 	// Get the jacobian
-	Vector3f p_j_temp;
+	Vector3d p_j_temp;
 	for(int i=0; i<3; i++) {
 		p_j_temp = p_j;
 		p_j_temp(i) = p_j_temp(i) + dx;
@@ -187,18 +205,18 @@ Matrix3f Listener::getJacobianB(Vector3f z, Vector3f p_i, Vector3f p_j) {
 }
 
 // Function for calculating relative measurements
-Vector3f Listener::getRelMeasurement(Vector3f p_i, Vector3f p_j) {
-	Vector2f v;
+Vector3d Listener::getRelMeasurement(Vector3d p_i, Vector3d p_j) {
+	Vector2d v;
 	v << (p_j(0) - p_i(0)), (p_j(1) - p_i(1));
-	Matrix2f R;
+	Matrix2d R;
 	R << cosf(p_i(2)), -sinf(p_i(2)), sinf(p_i(2)), cosf(p_i(2));
-	Vector2f v_rel = R.transpose() * v;
+	Vector2d v_rel = R.transpose() * v;
 
- 	Vector3f msg_out;
+ 	Vector3d msg_out;
 	msg_out(0) = v_rel(0);
 	msg_out(1) = v_rel(1);
 
-	float deltaAngle = (p_j(2) - p_i(2));
+	double deltaAngle = (p_j(2) - p_i(2));
 	if(deltaAngle > PI) {
 		deltaAngle = 2*PI - deltaAngle;
 	} else if(deltaAngle < -PI) {
@@ -215,10 +233,10 @@ nav_msgs::OccupancyGrid Listener::generateGridMap() {
 	nav_msgs::OccupancyGrid map;
 	// Get map information
 	float boundaryDistance = 1;		// [m]
-	float x_min = PG.row(0).minCoeff();
-	float x_max = PG.row(0).maxCoeff();
-	float y_min = PG.row(1).minCoeff();
-	float y_max = PG.row(1).maxCoeff();
+	float x_min = PG_opt.row(0).minCoeff();
+	float x_max = PG_opt.row(0).maxCoeff();
+	float y_min = PG_opt.row(1).minCoeff();
+	float y_max = PG_opt.row(1).maxCoeff();
 	// Define map meta data
 	map.info.resolution = 0.1;		// [m/cells]
 	map.info.width = (int)((x_max - x_min + 2.0f * boundaryDistance) / map.info.resolution);
@@ -229,12 +247,13 @@ nav_msgs::OccupancyGrid Listener::generateGridMap() {
 	// Resize the data structure
 	map.data.resize(map.info.width*map.info.height);
 	// Go over poses, starting with the pose of the first Loop Closure and ending with the last loop closure
-	int idx_min = LC.minCoeff();
-	int idx_max = LC.maxCoeff();
-	for(int i=idx_min-1; i<idx_max; i++) {
+	// int idx_min = LC.minCoeff();
+	// int idx_max = LC.maxCoeff();
+	// for(int i=idx_min-1; i<idx_max; i++) {
+	for(int i=0; i<idx_PG-1; i++) {
 		// Define the vector, TODO: Use Bresenham's line algorithm or something similar
-		Vector2f v; v << PG(0,i+1) - PG(0,i), PG(1,i+1) - PG(1,i);
-		Vector2f x; x << PG(0,i), PG(1,i);
+		Vector2f v; v << PG_opt(0,i+1) - PG_opt(0,i), PG_opt(1,i+1) - PG_opt(1,i);
+		Vector2f x; x << PG_opt(0,i), PG_opt(1,i);
 		Vector2f y;
 		for(int j=0; j<1001; j++) {
 			y = x + v * (((float)j)/1000.0f);
